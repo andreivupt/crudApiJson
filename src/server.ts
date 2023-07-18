833c07d32fc5ada4296c630928787f99d0f125ef
andrei <andrei.vupt@doberstein.com.br>
2023-07-18 15:45:17 -0300

    feat: delete method

--- src/server.ts
+++ src/server.ts
@@ -63,5 +63,28 @@ app.put('/api/user/:id', (req: any, res: any) => {
     res.send(`User with id ${userId} has been updated`)
 });
 
+app.delete('/api/user/:id', (req: any, res: any) => {
+    //atribui a base de dados em nova variavel
+    const jsonDataBase = fs.readFileSync(data);
+
+    //recupera o id enviado por parametro
+    const userId = req.params.id;
+    
+    //analisa string JSON e transforma em um objeto JavaScript
+    let content = JSON.parse(jsonDataBase);
+    
+    //delete
+    delete content[userId];
+    
+    //analisa um objeto em JavaScript e transforma em uma string JSON
+    const values = JSON.stringify(content);
+
+    //lê o arquivo da base de dados e adiciona o novo objeto
+    fs.writeFileSync(data, values);
+
+    //retorno amigável para o usuário que o endpoint
+    res.send(`User with id ${userId} has been deleted`);
+});
+
 //Iniciar servidor
 app.listen(3000, () => console.log(`listeniing at port: 3000`));
